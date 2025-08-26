174d47ab2c8d1dadb8738be66836bf75fbf93ec7
Jeremias Beyoglonian <you@example.com>
2025-08-26 15:54:07 -0300

    phaser

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-window.onload = function () { /**Esto hace que se ejecute la función una vez que termina de cargar la página**/
-    const params = new URLSearchParams(window.location.search); /**new se usa para crear nuevos objetos basados en una plantilla o prototipo**//**URLSearchParams(window.location.search) se utiliza para analizar la cadena de consulta de la URL actual (la parte que sigue al signo de interrogación '?') y crear un objeto que facilita la manipulación de los parámetros de consulta**/
-    const pais = params.get("pais"); /**params.get se utiliza para obtener el valor de un parámetro específico de la cadena de consulta de una URL utilizando la clase URLSearchParams**/
-    const tipo = params.get("tipo");
+import { Start } from './start.js';
 
-    const imagen = document.getElementById("imagen");
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: [Start]
+};
 
-    imagen.src = "aviónde" + pais + tipo + ".gif";
-    imagen.alt = "Imagen del avión de " + pais + " y del tipo " + tipo;
-}
+new Phaser.Game(config);
