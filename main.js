eacd402685ea3a540202e431eb86fae9e8b02477
Jeremias Beyoglonian <you@example.com>
2025-08-27 15:57:03 -0300

    corregí la animación del avión

--- main.js
+++ main.js
@@ -4,30 +4,36 @@ class Start extends Phaser.Scene {
   }
 
   preload() {
+
+    const params = new URLSearchParams(window.location.search);
+    this.pais = params.get("pais"); this.tipo = params.get("tipo");
+    
     this.load.image('background', 'fondosimple.png');
 
-    this.load.spritesheet('ship', 'animavióndeestadosunidosataque.png', { 
-      frameWidth: 30, 
-      frameHeight: 150 
-    });      
+    this.load.image('background', 'fondosimple.png');
+    this.load.spritesheet('avion', 'animavionde' + this.pais + this.tipo + '.png',
+    {
+      frameWidth: 32,
+      frameHeight: 32
+    });
   }
 
   create() {
     this.add.image(400, 300, 'background');
 
-    this.player = this.physics.add.sprite(400, 500, 'ship');
+    this.player = this.physics.add.sprite(400, 500, 'avion');
     this.player.setCollideWorldBounds(true);
 
     this.anims.create({
       key: 'idle',
-      frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 9 }),
+      frames: this.anims.generateFrameNumbers('avion', { start: 0, end: 5 }),
       frameRate: 10,
       repeat: -1
     });
     
     this.anims.create({
       key: 'attack',
-      frames: this.anims.generateFrameNumbers('ship', { start: 10, end: 19 }),
+      frames: this.anims.generateFrameNumbers('avion', { start: 6, end: 12 }),
       frameRate: 10,
       repeat: -1
     });
