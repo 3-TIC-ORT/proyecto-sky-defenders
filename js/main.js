c2d8cc3250d6e1ee5e7d4113f54ef20ae7d0a199
Jeremias Beyoglonian <you@example.com>
2025-09-09 15:41:09 -0300

    dos escenas

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ var tiempoBala = 0;
 var balas;
 var botonDisparo;
 
-class Start extends Phaser.Scene {
-  constructor() {
-    super('Start');
+class BaseLevel extends Phaser.Scene {
+  constructor(BaseDelJuego) {
+    super(BaseDelJuego);
   }
 
   preload() {
     const params = new URLSearchParams(window.location.search);
     this.pais = params.get("pais");
     this.tipo = params.get("tipo");
-    
+
     this.load.spritesheet('avion', '../imgs/animavionde' + this.pais + this.tipo + '.png', {
       frameWidth: 32,
       frameHeight: 32
@@ -44,18 +44,9 @@ class Start extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
     botonDisparo = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-    balas = this.physics.add.group({
-      defaultKey: 'bala',
-    });
-    this.enemigos = this.physics.add.group();
-    for (let row = 0; row < 3; row++) {
-      this.enemigos.createMultiple({
-        key: 'enemigo',
-        repeat: 9,
-        setXY: { x: 100, y: 100 + row * 80, stepX: 80 },
-        setScale: { x: 0.03, y: 0.03 }
-      });
-    }
+    balas = this.physics.add.group({ defaultKey: 'bala' });
+
+    this.spawnEnemies();
 
     this.tweens.add({
       targets: this.enemigos.getChildren(),
@@ -65,7 +56,7 @@ class Start extends Phaser.Scene {
       yoyo: true,
       repeat: -1
     });
-    
+
     this.physics.add.overlap(balas, this.enemigos, this.hitEnemigo, null, this);
   }
 
@@ -81,7 +72,7 @@ class Start extends Phaser.Scene {
     } else {
       this.player.anims.play('idle', true);
     }
-    
+
     if (botonDisparo.isDown && time > tiempoBala) {
       const bala = balas.get(this.player.x, this.player.y);
       if (bala) {
@@ -91,11 +82,51 @@ class Start extends Phaser.Scene {
         tiempoBala = time + 400;
       }
     }
+
+    if (this.enemigos.countActive(true) === 0) {
+      if (this.nextLevel === 'YouWin') {
+        window.location.href = 'YouWin.html';
+      } else {
+        this.scene.start(this.nextLevel);
+      }
+    }
   }
+
   hitEnemigo(bala, enemigo) {
     bala.destroy();
     enemigo.destroy();
-  }  
+  }
+}
+
+class Level1 extends BaseLevel {
+  constructor() {
+    super('Level1');
+    this.nextLevel = 'Level2';
+  }
+
+  spawnEnemies() {
+    this.enemigos = this.physics.add.group();
+    for (let row = 0; row < 3; row++) {
+      this.enemigos.createMultiple({
+        key: 'enemigo',
+        repeat: 9,
+        setXY: { x: 100, y: 100 + row * 80, stepX: 80 },
+        setScale: { x: 0.03, y: 0.03 }
+      });
+    }
+  }
+}
+
+class Level2 extends BaseLevel {
+  constructor() {
+    super('Level2');
+    this.nextLevel = 'YouWin';
+  }
+
+  spawnEnemies() {
+    this.enemigos = this.physics.add.group();
+    this.enemigos.create(683, 200, 'enemigo').setScale(0.2);
+  }
 }
 
 const config = {
@@ -107,7 +138,7 @@ const config = {
     default: 'arcade',
     arcade: { gravity: { y: 0 }, debug: false }
   },
-  scene: [Start]
+  scene: [Level1, Level2]
 };
 
 new Phaser.Game(config);
