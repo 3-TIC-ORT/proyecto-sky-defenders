aa7224272a7394f0d6938ae2e2a87aee89d3b3a7
AviFeder-byte <elduritoo123@gmail.com>
2025-10-17 15:30:35 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ import fs from 'fs';
 import { stringify } from 'querystring';
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 let PuntajeyNombre=
-subscribePOSTEvent("PuntajeyNombre"), () => {
-    return JSON.parse(fs.readFileSync("datos/puntajes.JSON", nuevoJson))
+subscribePOSTEvent ("PuntajeyNombre") () => {
+    return JSON.parse(fs.readFileSync("datos/puntajes.JSON","utf-8"))
   };
