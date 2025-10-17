8f6de60ba14c04eb0b25f6304b2cd3477c4905a8
AviFeder-byte <elduritoo123@gmail.com>
2025-10-17 12:05:32 -0300

    postpuntajeynombre

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,6 @@
 import fs from 'fs';
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+
+subscribePOSTEvent("PuntajeyNombre", () => {
+    return {};
+  });
