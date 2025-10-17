c0c93208c92cb40955e2524e329cf1cf95881436
AviFeder-byte <elduritoo123@gmail.com>
2025-10-17 15:18:09 -0300

    terminando post event putaje y nombre

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
+import { stringify } from 'querystring';
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 let PuntajeyNombre=
-subscribePOSTEvent("PuntajeyNombre", () => {
-    return {};
-  });
+subscribePOSTEvent("PuntajeyNombre"), () => {
+    return JSON.parse(fs.readFileSync("datos/puntajes.JSON", nuevoJson))
+  };
