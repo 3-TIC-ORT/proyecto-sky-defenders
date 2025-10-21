8d313db73761bb38740316ac8d3fa7d30c2fbfef
AviFeder-byte <elduritoo123@gmail.com>
2025-10-21 14:19:59 -0300

    tabla de puntajes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,32 @@
 import fs from 'fs';
 import { stringify } from 'querystring';
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
-let PuntajeyNombre=
-subscribePOSTEvent ("PuntajeyNombre") () => {
+
+subscribePOSTEvent ("PuntajeyNombre", () => {
     return JSON.parse(fs.readFileSync("datos/puntajes.JSON","utf-8"))
-  };
+  })
+
+
+  import {
+    //nombre y puntaje
+  } from "./dataEntry.js";
+  
+  
+  let PuntajeNuevo = {
+    "nombre":nombre,
+    "puntaje": puntaje,
+    
+  }
+  
+  let data = fs.readFileSync("data/puntajes.json","utf-8")
+  
+  let histPedidos = JSON.parse(data);
+  
+  HistPuntajes.push(PuntajeNuevo)
+  
+  let NuevoHistorialP = JSON.stringify(HistPuntajes, null, 2);
+  
+  fs.writeFileSync("data/pedidos.json", HistPuntajes.push );
+    
+  // Guardar pedido
+  // COMPLETEN USTEDES
