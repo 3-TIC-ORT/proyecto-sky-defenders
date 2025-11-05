e2aff22825ec8859323db86bd01aefd5859d0c95
Su Nombre <jerebeyo@gmail.com>
2025-11-05 10:54:35 -0300

    Ahora funciona con el tiempo del  jugador. Además si hay un empate en el puntaje se define por el tiempo

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ subscribePOSTEvent ("PuntajeyNombre", (datos) => {
   const usuario = datos.PyN.usuario;
 
   const puntaje = datos.PyN.puntaje;
+
+  const tiempo = datos.PyN.tiempo
   
 
   let PuntajeNuevo = {
     "nombre": usuario,
     "puntaje": puntaje,
-    
+    "tiempo": tiempo
   }
   
   let data = fs.readFileSync("datos/puntajes.json","utf-8")
@@ -20,7 +22,13 @@ subscribePOSTEvent ("PuntajeyNombre", (datos) => {
   let HistPuntajes = JSON.parse(data);
   
   HistPuntajes.push(PuntajeNuevo)
-  HistPuntajes.sort((a, b) => b.puntaje - a.puntaje);
+
+  HistPuntajes.sort((a, b) => {
+    if (b.puntaje === a.puntaje) {
+      return a.tiempo - b.tiempo;
+    }
+    return b.puntaje - a.puntaje;
+  });
 
   let NuevoHistorialP = JSON.stringify(HistPuntajes, null, 2);
   
