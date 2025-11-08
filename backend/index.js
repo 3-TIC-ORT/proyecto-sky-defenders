5a5a3e22f6399b46ffccb245c03812b2e63cc511
Jere <a@gamil.com>
2025-11-08 15:33:51 -0300

    === ;

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,31 @@
-import fs from 'fs';
-import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
-import { SerialPort } from 'serialport'
+import fs from "fs";
+import {
+  subscribeGETEvent,
+  subscribePOSTEvent,
+  realTimeEvent,
+  startServer,
+} from "soquetic";
+import { SerialPort } from "serialport";
 
 startServer(3000);
-subscribePOSTEvent ("PuntajeyNombre", (datos) => {
-    
+subscribePOSTEvent("PuntajeyNombre", (datos) => {
   const usuario = datos.PyN.usuario;
 
   const puntaje = datos.PyN.puntaje;
 
   const tiempo = datos.PyN.tiempo;
-  
 
   let PuntajeNuevo = {
-    "nombre": usuario,
-    "puntaje": puntaje,
-    "tiempo": tiempo
-  }
-  
-  let data = fs.readFileSync("datos/puntajes.json","utf-8")
-  
+    nombre: usuario,
+    puntaje: puntaje,
+    tiempo: tiempo,
+  };
+
+  let data = fs.readFileSync("datos/puntajes.json", "utf-8");
+
   let HistPuntajes = JSON.parse(data);
-  
-  HistPuntajes.push(PuntajeNuevo)
+
+  HistPuntajes.push(PuntajeNuevo);
 
   HistPuntajes.sort((a, b) => {
     if (b.puntaje === a.puntaje) {
@@ -32,53 +35,42 @@ subscribePOSTEvent ("PuntajeyNombre", (datos) => {
   });
 
   let NuevoHistorialP = JSON.stringify(HistPuntajes, null, 2);
-  
+
   fs.writeFileSync("datos/puntajes.json", NuevoHistorialP);
 
-  
-  return JSON.parse(fs.readFileSync("datos/puntajes.json","utf-8"))
-})
+  return JSON.parse(fs.readFileSync("datos/puntajes.json", "utf-8"));
+});
 
 const port = new SerialPort({
-  path: 'COM5', 
+  path: "COM5",
   baudRate: 9600,
 });
 
-
-
-  port.on('data', function (data) {
-  console.log('Data:',data.toString())
-   GuardarSeñales(data)
-})
+port.on("data", function (data) {
+  console.log("Data:", data.toString());
+  GuardarSeñales(data);
+});
 
 function GuardarSeñales(data) {
-  
-  const texto = data.toString().trim()
+  const texto = data.toString().trim();
 
-  if (texto == "led 1"){
-    fs.writeFileSync("señales.json","");
-  } 
-   else if(texto==="led 2"){
-   fs.writeFileSync("señales.json",JSON.stringify(""));
+  if (texto === "led 1") {
+    fs.writeFileSync("señales.json", "");
+  } else if (texto === "led 2") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
+  } else if (texto === "led 3") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
+  } else if (texto === "led 4") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
+  } else if (texto === "led 5") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
+  } else if (texto === "led 6") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
+  } else if (texto === "boton 1") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
+  } else if (texto === "boton 2") {
+    fs.writeFileSync("señales.json", JSON.stringify(""));
   }
-  else if(texto==="led 3"){
-   fs.writeFileSync("señales.json",JSON.stringify(""));
-   }
-   else if(texto==="led 4"){
-   fs.writeFileSync("señales.json",JSON.stringify(""));
-   }
-   else if(texto==="led 5"){
-    fs.writeFileSync("señales.json",JSON.stringify(""));
-   }
-   else if(texto==="led 6"){
-    fs.writeFileSync("señales.json",JSON.stringify(""));
-   }
-   else if(texto==="boton 1"){
-    fs.writeFileSync("señales.json",JSON.stringify(""));
-  }
-   else if(texto==="boton 2"){
-    fs.writeFileSync("señales.json",JSON.stringify(""));
-   }
-  }
+}
 const data = fs.readFileSync("señales.json", "utf-8");
 realTimeEvent("nuevaSeñal", data);
